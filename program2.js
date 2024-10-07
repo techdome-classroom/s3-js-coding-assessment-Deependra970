3e4c5bb9dd4f5bfabd9cddfcda4b1efea7d3af37
Deependra970 <deependratiwari970@gmail.com>
2024-10-07 15:48:24 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,8 +3,38 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let total = 0;
+    let prevValue = 0;
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        const currentValue = romanMap[s[i]];
+        
+        // If the current value is less than the previous value, we subtract
+        if (currentValue < prevValue) {
+            total -= currentValue;
+        } else {
+            // Otherwise, we add
+            total += currentValue;
+        }
+
+        // Update previous value
+        prevValue = currentValue;
+    }
+
+    return total;
 };
+
+
 
 
 module.exports={romanToInt}
